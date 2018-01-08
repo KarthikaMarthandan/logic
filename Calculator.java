ecddae35e7e87e8744daa73c1a45e42ea6bed95e
KarthikaMarthandan <34707597+KarthikaMarthandan@users.noreply.github.com>
2018-01-08 21:56:34 +0530

    Add files via upload

---
+++ Calculator.java
@@ -0,0 +1,13 @@
+package over;
+
+public class Calculator {
+	
+	 void sum (int a, int b)
+	 {
+	  System.out.println("sum is"+(a+b)) ;
+	 }
+	 void sum (float a, float b)
+	 {
+	  System.out.println("sum is"+(a+b));
+	 }
+}
---
+++ cal1.java
@@ -0,0 +1,16 @@
+package over;
+
+public class cal1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		 {
+		  Calculator  cal = new Calculator();
+		  cal.sum (8,5);      
+		  cal.sum (4.6f, 3.8f); 
+		 }
+		}
+
+	}
+
+
