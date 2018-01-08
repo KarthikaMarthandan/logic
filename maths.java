d50c9ce748164ae84b8a2366e018b50db9720d86
KarthikaMarthandan <34707597+KarthikaMarthandan@users.noreply.github.com>
2018-01-08 22:11:27 +0530

    Add files via upload

---
+++ maths.java
@@ -0,0 +1,18 @@
+package satt;
+
+public class maths
+{
+	static double add(double a,double b)
+	{
+	return(a+b);	
+	}
+	static double sub(double a,double b)
+	{
+		return(a-b);
+	}
+	static double mul(double a,double b)
+	{
+		return(a*b);
+	}
+
+}
---
+++ maths1.java
@@ -0,0 +1,16 @@
+package satt;
+
+public class maths1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		double x=maths.add(24.12, 12.4);
+		System.out.println("addition="+x);
+		double y=maths.sub(24.12, 12.4);
+		System.out.println("SUBRACTION="+y);
+		double z=maths.mul(2, 12);
+		System.out.println("Multiplication="+z);
+
+	}
+
+}
